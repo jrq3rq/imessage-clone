fcdb7b668f228c85552c3dac41fa77c9e8da2ef9
YOUR NAME <YOUR EMAIL ADDRESS>
2020-10-12 14:14:43 -0700

    foundation sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Imessage from "./Imessage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Imessage clone</h1>
+    <div className="app">
+      <Imessage />
     </div>
   );
 }
