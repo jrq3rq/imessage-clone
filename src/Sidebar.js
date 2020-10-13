bb4df66f69042e3bd5e6a317a8e27c799aed9616
YOUR NAME <YOUR EMAIL ADDRESS>
2020-10-13 16:49:23 -0700

    signout using avatar added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,12 +4,20 @@ import "./Sidebar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import SidebarChat from "./SidebarChat.js";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar className="sidebar__avatar" />
+        <Avatar
+          onClick={() => auth.signOut()}
+          src={user.photo}
+          className="sidebar__avatar"
+        />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
